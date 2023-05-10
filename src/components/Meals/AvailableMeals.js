56381742c0eaa8e88da2d74fefc6d40ba58fbc7e
alex <o.lototskyi@gmail.com>
2023-05-10 09:26:23 +0300

    loading state and catching fetch errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -10,10 +10,17 @@ import classes from './AvailableMeals.module.css';
 const AvailableMeals = () => {
 
     const [meals, setMeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState();
 
     useEffect(() => {
         const fetchMeals = async () => {
             const response = await fetch('https://react-http-46560-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+            
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+            
             const responseData = await response.json();
 
             const loadedMeals = [];
@@ -28,11 +35,26 @@ const AvailableMeals = () => {
             }
 
             setMeals(loadedMeals);
+            setIsLoading(false);
         };
 
-        fetchMeals();
-        
+        fetchMeals().catch(error => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        });
     }, []);
+
+    if (isLoading) {
+        return <section className={classes.MealsLoading}>
+            <p>Loading...</p>
+        </section>
+    }
+
+    if (httpError) {
+        return <section className={classes.MealsError}>
+        <p>{httpError}</p>
+    </section>
+    }
 
     const mealsList = meals.map(meal => (
     <MealItem
